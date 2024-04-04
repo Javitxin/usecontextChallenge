e428ce07603e7d30870031adaf40b6b10e9be643
javi <javitxin1977@gmail.com>
2024-04-04 12:40:54 +0200

    Endig up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css'; // Archivo de estilos CSS
+import { ThemeProvider } from './themes/ThemeContext.jsx';
+import RoutesApp from './routes/RoutesApp.jsx';
+
 
 const App = () => {
   return (
-<></>
+    <ThemeProvider>
+      <RoutesApp />
+    </ThemeProvider>
   );
 };
 
